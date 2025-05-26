eda821b7a15fb80aba7258efd435994217281fd7
foxstudent106431 <t.ulukbek01@gmail.com>
2025-05-26 23:38:14 +0300

    init cobra project

---
+++ main.go
@@ -0,0 +1,13 @@
+/*
+Pookie — simple http client.
+Copyright © 2025 Toichuev Ulukbek t.ulukbek01@gmail.com
+
+Licensed under the MIT License.
+*/
+package main
+
+import "github.com/Ulukbek-Toichuev/pookie/cmd"
+
+func main() {
+	cmd.Execute()
+}
