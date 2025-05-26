eab58bfca5a34e1509ac3968fd183e8dbcc49138
foxstudent106431 <t.ulukbek01@gmail.com>
2025-05-27 01:36:08 +0300

    update

--- cmd/root.go
+++ cmd/root.go
@@ -4,32 +4,23 @@ Copyright © 2025 Toichuev Ulukbek t.ulukbek01@gmail.com
 
 Licensed under the MIT License.
 */
+
 package cmd
 
 import (
-	"os"
-
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "pookie",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "simple http client.",
+	Long:  "simple http client.",
 }
 
 func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
-	}
+	cobra.CheckErr(rootCmd.Execute())
 }
 
 func init() {
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.AddCommand(GetCmd())
 }
